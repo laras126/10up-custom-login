03d88395e530b768d86b4d1a35aabe68b4633573
laras126 <laras126@gmail.com>
2017-06-22 15:37:44 -0700

    stop animation with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
 
 document.addEventListener('DOMContentLoaded', function(){
+
+  // Remember Me is checked by default
   document.getElementById('rememberme').checked = true;
+
+  // Add class to stop animation once complete
+  var logo = document.querySelector('.logo');
+  logo.addEventListener('animationend', function() {
+    logo.classList.add('js-complete');
+  });
+
 });
